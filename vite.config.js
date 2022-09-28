48844ef52f04e9ccd91a248126bab8cbba869ac3
余智民 <791369498@qq.com>
2022-09-28 16:59:04 +0800

    update package

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import unocss from 'unocss/vite';
-import presetMini from '@unocss/preset-mini'
-import presetIcons from '@unocss/preset-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,22 +15,6 @@ export default defineConfig({
         reactivityTransform: true,
       }
     }),
-    unocss({
-      presets: [
-        /*
-        https://icon-sets.iconify.design/fa
-        https://icon-sets.iconify.design/fa6-solid
-        https://icon-sets.iconify.design/fa6-regular
-        https://icon-sets.iconify.design/fa6-brands
-        https://icon-sets.iconify.design/ant-design
-         */
-        presetIcons({}),
-        (() => {
-          const preset = presetMini()
-          preset.preflights = []
-          return preset
-        })(),
-      ],
-    }),
+    unocss(),
   ],
 })
